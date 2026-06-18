b2fb84fa56e49315f7407ccd88147e57277f4978
Abubakar work <idrisabr266@gmail.com>
2026-06-18 11:39:58 +0100

    new prod.py settings added

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -1,27 +1,21 @@
+import os
 import dj_database_url
 from storefront.settings.common import *
-import os
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-ALLOWED_HOSTS = []
-
-
-
+ALLOWED_HOSTS = [os.environ.get('RAILWAY_PUBLIC_DOMAIN', '')]
 
 DATABASES = {
     'default': dj_database_url.config(
         env='DATABASE_URL',
-        conn_max_age=600
+        conn_max_age=600,
     )
 }
 
-
-REDIS_URL = os.environ['REDIS_URL']
-
+REDIS_URL = os.environ.get('REDIS_URL', '')
 
 CELERY_BROKER_URL = f"{REDIS_URL}/0"
 
@@ -29,22 +23,13 @@ CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
         "LOCATION": f"{REDIS_URL}/1",
-        'TIMEOUT': 10 * 60, # 10 minutes
+        "TIMEOUT": 10 * 60,
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
     }
 }
 
-
-# EMAIL_HOST = os.environ['MAILGUN_SMTP_SERVER']
-# EMAIL_HOST_USER = os.environ['MAILGUN_SMTP_LOGIN']
-# EMAIL_HOST_PASSWORD = os.environ['MAILGUN_SMTP_PASSWORD']
-# EMAIL_PORT = os.environ['MAILGUN_SMTP_PORT']
-
-
-
-# Using .get() prevents KeyError crashes if a variable is missing
 EMAIL_HOST = os.environ.get('MAILGUN_SMTP_SERVER', 'smtp.mailgun.org')
 EMAIL_HOST_USER = os.environ.get('MAILGUN_SMTP_LOGIN', '')
 EMAIL_HOST_PASSWORD = os.environ.get('MAILGUN_SMTP_PASSWORD', '')
