e84bb0d0cd9d4aa26420858326b2d4ddb088ebc3
Abubakar work <idrisabr266@gmail.com>
2026-06-30 15:30:19 +0100

    fix: add missing CMD to dockerfile, use gunicorn with dynamic port

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,8 @@
+
 #!/bin/bash
 
-# Apply database migrations
 echo "Apply database migrations"
 python manage.py migrate
 
-# Start server
 echo "Starting server"
-python manage.py runserver 0.0.0.0:8000
+gunicorn storefront.wsgi --bind 0.0.0.0:${PORT:-8000}
